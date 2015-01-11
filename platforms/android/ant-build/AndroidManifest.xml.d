6f038facadc46950563d95fb05d139a88b084f54
tadeussenf <tadeussenf@fastmail.fm>
2015-01-12 00:38:41 +0100

    Big UI and scanner update

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\rly\Documents\GitHub\hdm-beacon\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\rly\Documents\GitHub\hdm-beacon\platforms\android\AndroidManifest.xml \
+C:\Users\rly\Dropbox\HDM_Stuttgart\iBeacons\hdm-beacon\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\rly\Dropbox\HDM_Stuttgart\iBeacons\hdm-beacon\platforms\android\AndroidManifest.xml \
